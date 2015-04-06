1a78ec3e5315a09fa8d668854beb3250c76559a7
Zohaib Tariq <mr.zohaibt@gmail.com>
2015-04-06 07:25:13 -0700

    another controller added

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,5 +1,23 @@
 var app = angular.module("phoneDirectory", []);
 
-app.controller("userName", function($scope){
+app.controller("user", function($scope){
     $scope.name = "Zohaib Tariq";
+    $scope.countries = [
+    	"Pakistan",
+    	"India",
+    	"UK",
+    	"USA"
+    ];
+});
+
+app.controller("countriesList", function($scope){
+	$scope.totalPopulation = 125;	
+    $scope.countries = [
+    	{name : "Pakistan",population: 18},
+    	{name : "India",population: 20},
+    	{name : "Iran",population: 15}
+    ];
+    $scope.populationPercentage = function(countryPopulation){
+    	return ((countryPopulation/$scope.totalPopulation)*100);
+    };
 });
